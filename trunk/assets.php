fe238d254e6c60d8187809faa48c7811538f6c5f
artvolk <artvolk@036c84f9-cef9-117a-7063-a41b8d8c7503>
2010-10-03 14:19:23 +0000

    Initial commit

---
+++ trunk/assets.php
@@ -0,0 +1,31 @@
+<?php
+	$CP_ASSETS = array(
+		'css-default' => array(
+			'type' => 'css',
+			'files' => array(
+				'jquery.ui.core.css',
+				'jquery.ui.resizable.css',
+				'jquery.ui.selectable.css',
+				'jquery.ui.accordion.css',
+				'jquery.ui.autocomplete.css',
+				'jquery.ui.button.css',
+				'jquery.ui.dialog.css',
+				'jquery.ui.slider.css',
+				'jquery.ui.tabs.css',
+				'jquery.ui.datepicker.css',
+				'jquery.ui.progressbar.css',
+				'jquery.ui.theme.css',
+			)
+		),
+		'js-default' => array(
+			'type' => 'javascript',
+			'files' => array(
+				'jquery-1.4.2.min.js',
+				'external/jquery.bgiframe-2.1.1.js',
+				'external/jquery.cookie.js',
+				'external/jquery.metadata.js',
+				'jquery-ui-1.8.5.custom.js',
+			)
+		),
+	);
+?>
